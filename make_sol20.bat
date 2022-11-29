17c33ded1007495716ab7836bf5615cb8cc5ef2b
nino-porcino <nino.porcino@gmail.com>
2022-11-29 11:42:41 +0100

    make it work on 16K RAM SOL-20

--- make_sol20.bat
+++ make_sol20.bat
@@ -1,7 +1,7 @@
 @echo off
 
 cd src
-zcc +sol20 -pragma-define CRT_ORG_CODE=256 tetris.c -o ..\out\tetris_sol20.com -DZCC -DSOL20 -create-app
+zcc +sol20 -pragma-define:REGISTER_SP=16383 -pragma-define CRT_ORG_CODE=256 tetris.c -o ..\out\tetris_sol20.com -DZCC -DSOL20 -create-app
 cd ..
-call node tools/mkent out\tetris_sol20.com 0100 > out\tetris_sol20.ent
+call node tools\mkent out\tetris_sol20.com 0100 > out\tetris_sol20.ent
 
